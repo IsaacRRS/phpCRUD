e0ca869a60076c3aa2ca6d3debd6f309dfd2d8d3
Isaac <zackhyou@gmail.com>
2024-09-09 15:51:30 -0300

    ➕  - Bootstrap

--- index.php
+++ index.php
@@ -4,8 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD - Funcionamento</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 
+    <div class="container my-5">
+        <h3>Lista de pessoas</h3>
+        <a class="btn btn-primary" href="criar.php">Adicionar</a>
+    </div>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Celular</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
 </body>
 </html>
